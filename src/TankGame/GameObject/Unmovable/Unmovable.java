1610b62bd679a8b4e60b4ea3c03c3a0c99578313
ArunaNuwantha <e16261@eng.pdn.ac.lk>
2021-08-06 19:09:11 +0530

    initial comment

---
+++ src/TankGame/GameObject/Unmovable/Unmovable.java
@@ -0,0 +1,14 @@
+
+package TankGame.GameObject.Unmovable;
+
+import TankGame.GameObject.GameObject;
+
+import java.awt.image.BufferedImage;
+
+public class Unmovable extends GameObject {
+
+    public Unmovable(int x, int y, int width, int height, BufferedImage img) {
+        super(x, y, width, height, img);
+    }
+    
+  }
